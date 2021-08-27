9442437948985548efd7ad1e46c7f397c81dabf0
Joseph Burger <joseph@airkit.com>
2021-08-26 17:18:02 -0700

    added failing tests

---
+++ my-app/src/lib/random-id-generator.ts
@@ -0,0 +1,12 @@
+export const WORD_SEPARATOR = "-_.,"
+
+const randomWords: string[] = [
+    "apple", "table", "beans", "ryan"
+]
+
+//  "apple-beans" "ryan_table"
+function randomIdGenerator(): string {
+  return "";
+}
+
+export default randomIdGenerator;
--- my-app/src/lib/some-algorithm.spec.ts
+++ my-app/src/lib/some-algorithm.spec.ts
@@ -1,13 +1,20 @@
-import someAlgorithm from "./some-algorithm";
+import randomIdGenerator from "./random-id-generator";
+import { WORD_SEPARATOR } from "./random-id-generator";
 
-describe("someAlgorithm", function () {
+const MINIMUM_ID_LENGTH: number = 8
 
 
+describe("random id generator", function () {
+  it("should generate a random id", function () {
+    const aRandomId1: string = randomIdGenerator()
+    expect(aRandomId1.length).toBeGreaterThanOrEqual(MINIMUM_ID_LENGTH)
+  });
 
-  describe("when the algorithm runs", function () {
+  // "ryan-is-cool" ->
+  it('should generate an id with multiple words', function () {
+    const aRandomId1: string = randomIdGenerator()
 
-    it("should be correct", function () {
-      expect(someAlgorithm()).toEqual(3);
-    });
+    const parts = aRandomId1.split(new RegExp(WORD_SEPARATOR)) // ["ryan", "is", "cool"]
+    expect(parts.length).toBeGreaterThanOrEqual(2)
   });
 });
