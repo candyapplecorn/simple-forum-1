ecf74cbb70eb393951f94966dcd3ec235e69ad77
Joseph Burger <joseph@airkit.com>
2021-08-27 12:18:26 -0700

    adjusted css, removed some inputs

--- my-app/src/components/side-menu.tsx
+++ my-app/src/components/side-menu.tsx
@@ -6,31 +6,12 @@ interface SideMenuProps {
 }
 
 function SideMenu(props: SideMenuProps) {
-  const [board, findBoard] = useState("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    alert(`Submitting Search ${board}`);
-  };
   return (
     <div className={styles.menustuff}>
       <header>
         <form>
-          <h3> Find a board </h3>
-          {handleSubmit}
-          <input
-            type="text"
-            value={board}
-            onChange={(e) => {
-              debugger
-              findBoard(e.target.value);
-              props.setQuery(e.target.value)
-            }}
-          />
-          <input type="submit" value="find that board" />
-          <br />
-          <br />
-          <button> Create Board </button>
+          <h3>Search Boards</h3>
+          <input type="text" onChange={(e) => props.setQuery(e.target.value)} />
         </form>
       </header>
     </div>
