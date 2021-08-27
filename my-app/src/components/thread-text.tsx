5d180235a89bc9ef27e654d3674d041f9e3d8de8
Austin Rafter <austin.rafter@sjsu.edu>
2021-08-27 09:17:42 -0700

    Changed thread component to show the properly named thread sub-components subject and comments. added thread text component.

---
+++ my-app/src/components/thread-text.tsx
@@ -0,0 +1,27 @@
+import mockData from "../mock-data/mock-board-data";
+function ThreadText() {
+/*need to figure out how to conditionally pull an item
+ from the map. would need to pass the string from either
+ the pressed button on main page, the entered subject from
+ the search, or the created thread.
+ */
+  return (
+    <div>
+      <header>
+        <div>
+
+
+          {mockData.map((item, i) => (
+            <tr key={i}>
+              <p>{item.title}</p>
+              <br />
+              <hr />
+            </tr>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default ThreadText;
--- my-app/src/components/thread.tsx
+++ my-app/src/components/thread.tsx
@@ -2,12 +2,14 @@ import mockData from "../mock-data/mock-board-data";
 import styles from "./home.module.css";
 import ThreadSubject from "./thread-subject";
 import ThreadComments from "./thread-comments";
+import ThreadText from "./thread-text";
 
 function Board() {
   return (
     <div className={styles.homepage}>
       <div className={styles.test}>
         <ThreadSubject  />
+        <ThreadText />
         <ThreadComments />
       </div>
     </div>
